abf7f9dac53ed888c3ebf088a9eefd7d00bcc4e6
viral vaghela <viralvaghela7109@gmail.com>
2018-11-19 18:59:17 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,134 @@
+	<!-- PHP CODE FOR GENERATE AMAZING THOUGH -->
+<?php
+	$thought=array('1' => "Talk is cheap. Show me the code",
+					'2' => "Programs must be written for people to read, and only incidentally for machines to execute.",
+					'3' => "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
+					'4'	=> "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning",
+					'5' => "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.",
+					'6' =>"My religion is very simple. My religion is kindness",
+					'7' => "My religion is very simple. My religion is kindness",
+					'8' => "Kindness is a source of relief to the soul of the giver, creating a sense of fortitude that is incomprehensible to those who do not know what kindness is all about.",
+					'9' => "Life is really generous to those who pursue their personal legend.",
+					'10'=>"You have to chase your dreams, no matter what. The impossible just takes a little longer. One stroke at a time, one step at a time, the impossible is easy to achieve.",
+					'11'=>"Don’t keep your dreams in your eyes, they may fall as tears. Keep them in your heart so that every heartbeat may remind you to convert them into reality.",
+					'12'=>"What one programmer can do in one month, two programmers can do in two months.",
+					'13'=>"Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program",
+					'14'=>" Its hard enough to find an error in your code when you're looking for it; it's even harder when you've assumed your code is error-free.",
+					'16'=>"First, solve the problem. Then, write the code.",
+					'17'=>"Linux is only free if your time has no value.",
+					'18'=>"Beware of bugs in the above code; I have only proved it correct, not tried it",
+					'19'=>"My code never has bugs, it just develops random unexpected features.",
+					'20'=>"A programmer is a person who fixed a problem that you don’t know you have, in a way you don’t understand.",
+					'21'=>"Linux is only free if your time has no value.",
+					'22'=>"If at first you don’t succeed, you must be programmer.",
+					'23'=>"If God is a programmer then there is no confusion Devil is a hacker.",
+					'24'=>"Eat, Sleep, Code, Repeat.",
+					'25'=>"Programming is thinking, not typing.",
+					'26'=>"The only people up at 3am are either in love, lonely, drunk or programmer",
+					'27'=>"I'm not anti-social, I'm just not user friendly",
+					'28'=>"Travel to life is like css to html",
+					'29'=>"Any fool can use a computer. Many do",
+					'30'=>"When Life Gives You Questions, Google has Answers and me also ;) .",
+					'31'=>"The more I C, the less I see",
+					'32'=>"Success is going from failure to failure without losing enthusiasm",
+					'33'=>"Success is doing ordinary things extraordinarily well.",
+					'34'=>"Success means having the courage, the determination, and the will to become the person you believe you were meant to be.",
+					'35'=>"Success is one percent inspiration, ninety-nine percent perspiration.",
+					'36'=>"Do or do not. There is no try.",
+					'37'=>"Fear regret more than failure.",
+					'38'=>"Success comes down to hard work plus passion, over time.",
+					'39'=>"We have no ability to be equal to all have opportunities equal to their ability lane.",
+					'40'=>"You have to dream before the dream comes true.",
+					'41'=>"You cannot change your future, but you can change your habits, and surely your habits will change your future",
+					'42'=>"Your favorite...Success is when your signature turns into your autograph",
+					'43'=>"Life is a difficult game. You can win it only by retaining your birthright to be a person",
+					'44'=>"Man needs difficulties because to enjoy the success that they need to",
+					'45'=>"Love Your Job but don’t love your company, because you may not know when your company stops loving you.",
+					'46'=>"It is very Easy To Defeat Someone, But it is Very Hard To Win Someone.",
+					'47'=>"Love is self-acknowledgement. I AM loved because I deserve to be loved.",
+					'48'=>"Love gives more than it takes. It is often opposed to human nature",
+					'49'=>"Love does not slip away. It walks away … with dignity",
+					'50'=>"Love always sings a song in the midst of chaos.",
+					'51'=>"Life would be so much easier if we only had the source code",
+					'52'=>"Programmer is just a tool which transforms caffeine into code",
+					'53'=>"खुन मे तेरे coding
+Coding मे तेरा खुन
+
+खुन मे तेरे coding
+
+Coding मे तेरा खुन
+
+ऊपर error
+
+नीचे warning
+
+बीच मे तेरा जुनून
+
+Ayeee developer",
+					'54'=>"Wrong decisions in life are like exception/error in programe, we most review the sample code to overcome with that error in next iteration of execution ....",
+					'55'=>" select * from life;",
+					'56'=>"Google Always One Step behind Me",
+					'57'=>"I speak my mind. I never mind what I speak",
+					'58'=>"I need Google in my brain.",
+					'59'=>"I love my job only when I’m on vacation",
+					'60'=>"I  never dreamed about success. I worked for it.",
+					'61'=>"If you’re bad then call me your DAD",
+					'62'=>"Fake people have an image to maintain REAL PEOPLE just don’t care.",
+					'63'=>"I really hate saying sorry. But when I do, I honestly do mean it.",
+					'64'=>"Don’t call me lucky. I’m a hard worker",
+					'65'=>"Life’s a journey not a race.",
+					'66'=>"if you fail to manage time read this...We are human not a robot,,",
+					'67'=>"A comfort zone is a beautiful place, but nothing ever grows there.",
+					'68'=>"Smartness is a perfect beauty",
+					'69'=>"I’M Like Monday. Nobody Likes Me",
+					'70'=>"No matter how good you are, you can always be replaced.",
+					'71'=>"I am… stubborn, and I admit it, so it’s OK.",
+					'72'=>"I don’t hold grudges, I remember facts",
+					'73'=>"Kidnapping? I prefer the term surprise adoption",
+					'74'=>"You only live once, so do everything twice.",
+					'75'=>"while (success!=true) { keepMovingForward(); }",
+					'76'=>"hello world",
+					'77'=>"Life has no ctrl+z",
+					'78'=>"A computer once beat me at chess, but it was no match for me at kick boxing.",
+					'79'=>"When you play the Game of Codes, you execute or you fail. There is no middle ground.",
+					'80'=>"Best practices for Java - { use something else; }",
+					'81'=>"Hey girl, without you my world is null;",
+					'82'=>"There’s no place like 127.0.0.1",
+					'83'=>"I'm just interface, I don't know how to implement me...this is java's topic",
+					'84'=>"Intensive coding, Don’t disturb :P",
+					'85'=>"Don't try to inherit my class,
+You cannot handle the exception I throw",
+					'86'=>"Don’t compare yourself with anyone in this world… if you do so, you are insulting yourself.",
+					'87'=>"Patience is a key element of success.",
+					'88'=>"It’s fine to celebrate success, but it is more important to heed the lessons of failure.",
+					'89'=>"To win big, you sometimes have to take big risks.",
+					'90'=>"If you think your teacher is tough, wait till you get a boss.",
+					'91'=>"Don’t let the noise of others’ opinions drown out your own inner voice.",
+					'92'=>"Creativity is just connecting things.",
+					'93'=>"Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations.",
+					'94'=>"Your time is limited, so don’t waste it living someone else’s life",
+					'95'=>"Stay hungry. Stay foolish.",
+					'96'=>"It is Important to Follow your Dreams and Heart. Do Something that Excites You.",
+					'97'=>" As a leader, It is important to not just see your own success, but focus on the success of others",
+					'98'=>"A person who is happy is not because everything is right in his life, He is happy because of his attitude towards everything in his life is right.",
+					'99'=>"it is always good to work with people who make you feel insecure about yourself. That way, you will constantly keep pushing your limits.",
+					'100'=>"Wear your failure as a badge of honor!"
+
+ 		 );
+	$see= $thought[rand(1,100)];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Random Quote Generator</title>
+</head>
+<body style="background: #c1c1c1;">
+	<center>
+		<b style="color: blue;font-size: 50px">
+			<?php
+					echo $see;
+			?>
+		</b>
+	</center>
+</body>
+</html>
